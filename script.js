c62616fcdf984d0d0dd67cd9ff1187c749cbdf5b
saad <saadansari74002@gmail.com>
2023-05-18 15:45:20 +0530

    making a front-end

---
+++ script.js
@@ -0,0 +1,10 @@
+const billInput = document.getElementById('billTotalInput')
+const tipInput = document.getElementById('tipInput')
+const numberOfPeopleDiv = document.getElementById('numberOfPeople')
+const perPersonTotalDiv = document.getElementById('perPersonTotal')
+
+let numOfPerson = Number(numberOfPeopleDiv.innerText)
+
+const calculateBill = () => {
+
+} 
